3a4bba49fbd056231dbe29c71b5cb168fbb3c3c3
DRedDevil04 <devamydesai@gmail.com>
2025-03-02 18:42:40 +0530

    test interface

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 from interface.connector import SSHConnector
+
+ssh=SSHConnector("192.168.122.93","kali","kali")
+
+ssh.create_ssh_session()
+command='uname -a'
+output = ssh.execute_command(command)
+
+# Print the output
+print("Command Output:")
+print(output)
