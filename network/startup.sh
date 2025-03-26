24ce21ccb9dfb278a8dd70a56ca4cd45250bf6cf
Nitu Sherawat <sherawatnitu49@gmail.com>
2025-03-26 15:14:58 +0530

    automate network startup

---
+++ network/startup.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+sudo virsh net-start dev
+sudo virsh start kali
+sudo virsh start node1-linux
+sudo virsh start node2-linux
+sudo virsh start node3-linux
+sudo iptables -t nat -A POSTROUTING -s 192.168.133.0/24 -d 192.168.122.0/24 -o virbr0 -j MASQUERADE
